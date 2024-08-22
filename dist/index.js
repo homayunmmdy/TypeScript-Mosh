833ac77ffc8422c06cb2a87d72df77cffa8018b9
Homayoun <homayoun763@gmail.com>
2024-08-22 15:13:40 +0330

    Access Controll Keywords

--- dist/index.js
+++ dist/index.js
@@ -3,18 +3,24 @@ class Account {
     constructor(id, owner, balance) {
         this.id = id;
         this.owner = owner;
-        this.balance = balance;
+        this._balance = balance;
     }
     deposite(amount) {
         if (amount <= 0) {
             throw new Error("Invalid amounr");
         }
         else {
-            this.balance += amount;
+            this._balance += amount;
         }
+    }
+    calculateTax() {
+        return this._balance - 1.2;
+    }
+    getBalance() {
+        return this.calculateTax();
     }
 }
 let account = new Account(1, "Homayoun", 0);
 account.deposite(100);
-console.log(account.balance);
+console.log(account.getBalance());
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,32 @@
 class Account {
   readonly id: number;
   owner: string;
-  balance: number;
+  private _balance: number;
   nickname?: string;
 
   constructor(id: number, owner: string, balance: number) {
     this.id = id;
     this.owner = owner;
-    this.balance = balance;
+    this._balance = balance;
   }
 
   deposite(amount: number): void {
     if (amount <= 0) {
       throw new Error("Invalid amounr");
     } else {
-      this.balance += amount;
+      this._balance += amount;
     }
+  }
+
+  private calculateTax() {
+    return this._balance - 1.2;
+  }
+
+  getBalance(): number {
+    return this.calculateTax();
   }
 }
 
 let account = new Account(1, "Homayoun", 0);
 account.deposite(100);
-console.log(account.balance);
+console.log(account.getBalance());

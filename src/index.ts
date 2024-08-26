336225ed073f289e02c30f198837248314048ef6
Homayoun <homayoun763@gmail.com>
2024-08-26 19:33:38 +0330

    Parameterized Decorators

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
-function Component(constructor: Function) {
-  console.log("Components decorator called");
-  constructor.prototype.uniqueId = Date.now();
-  constructor.prototype.insertInDOM = () => {
-    console.log("Insertign the components in the DOM")
-  }
+type ComponentOptions = {
+  selector: string;
+};
+
+function Component(options: ComponentOptions) {
+  return (constructor: Function) => {
+    console.log("Components decorator called");
+    constructor.prototype.options = options;
+    constructor.prototype.uniqueId = Date.now();
+    constructor.prototype.insertInDOM = () => {
+      console.log("Insertign the components in the DOM");
+    };
+  };
+}
+
+@Component({selector: '#my-profile'})
+class ProfileComponents {
 }

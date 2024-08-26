65841cc95d47d7089319fc5322f725506a40d2ee
Homayoun <homayoun763@gmail.com>
2024-08-27 00:58:16 +0330

    calling data useing Effect Hook

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,11 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ReminderList from "./components/ReminderList";
 import Reminder from "./models/Reminder";
+import ReminderService from "./services/reminder";
 
 const App = () => {
-  const [reminders, setReminders] = useState<Reminder[]>([
-    { id: 1, title: "Reminder 1" },
-  ]);
+  const [reminders, setReminders] = useState<Reminder[]>([]);
+
+  useEffect(() => {
+    loadReminders();
+  }, []);
+
+  const loadReminders = async () => {
+    const reminders = await ReminderService.getReminders();
+    setReminders(reminders);
+  };
+
   return (
     <>
       <ReminderList items={reminders} />
--- react-typescript/src/components/ReminderList.tsx
+++ react-typescript/src/components/ReminderList.tsx
@@ -6,9 +6,9 @@ interface ReminderListProps {
 
 const ReminderList = ({ items }: ReminderListProps) => {
   return (
-    <ul>
+    <ul className="list-group">
       {items.map((item) => (
-        <li key={item.id}>{item.title}</li>
+        <li className="list-group-item" key={item.id}>{item.title}</li>
       ))}
     </ul>
   );

19bb1aa37ce089e79fd8f893c4b8aeac44a8ccc4
Homayoun <homayoun763@gmail.com>
2024-08-27 00:33:27 +0330

    Add UseState to reminderLists

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import ReminderList from "./components/ReminderList";
 import Reminder from "./models/Reminder";
 
-const reminders: Reminder[] = [{ id: 1, title: "Reminder 1" }];
 const App = () => {
+  const [reminders, setReminders] = useState<Reminder[]>([
+    { id: 1, title: "Reminder 1" },
+  ]);
   return (
     <>
       <ReminderList items={reminders} />

07381b221b703b53d0215693fbc30e9fe0f290f6
Homayoun <homayoun763@gmail.com>
2024-08-27 21:24:05 +0330

    Create router

--- reminders-api/index.ts
+++ reminders-api/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import reminders from "./routers/reminders";
 
 const app = express();
 
-app.get('/', (req,res) => {
-    res.send('Hello World')
-})
+app.use("/reminders", reminders);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 app.listen("8000", () => console.log("Server startedd"));
